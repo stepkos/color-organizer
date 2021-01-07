4d3f66e620a9a8140691cc02bd21070a09aba0a2
stepkos <52202262+stepkos@users.noreply.github.com>
2021-01-07 17:36:59 +0100

    add delete

--- autoCopy.js
+++ autoCopy.js
@@ -4,58 +4,58 @@ const header = document.querySelector("header h1");
 // Anti animation spam boolean
 var headerAnim = false;
 
-function headerTextAnimation(text) {
-    // 0ms - Fade out
-    header.style.opacity = 0;
+// function headerTextAnimation(text) {
+//     // 0ms - Fade out
+//     header.style.opacity = 0;
 
-    // 300ms - Fade in
-    setTimeout(() => {
-        header.innerHTML = text;
-        header.style.opacity = 1;
-    }, 300);
+//     // 300ms - Fade in
+//     setTimeout(() => {
+//         header.innerHTML = text;
+//         header.style.opacity = 1;
+//     }, 300);
 
-    // 2000ms - Fade out
-    setTimeout(() => {
-        header.style.opacity = 0;
-    }, 2000);
+//     // 2000ms - Fade out
+//     setTimeout(() => {
+//         header.style.opacity = 0;
+//     }, 2000);
   
-    // 2300ms - Fade in
-    setTimeout(() => {
-        header.innerHTML = "Color Organizer";
-        header.style.opacity = 1;
-    }, 2300);
-}
+//     // 2300ms - Fade in
+//     setTimeout(() => {
+//         header.innerHTML = "Color Organizer";
+//         header.style.opacity = 1;
+//     }, 2300);
+// }
 
-function promiseAnimation(text) {
-    header.style.opacity = 0;
+// function promiseAnimation(text) {
+//     header.style.opacity = 0;
 
-    new Promise(resolve => {
-        setTimeout(() => {
-            header.innerHTML = text; 
-            header.style.opacity = 1;
-            resolve()
-        }, 300);
-    })
+//     new Promise(resolve => {
+//         setTimeout(() => {
+//             header.innerHTML = text; 
+//             header.style.opacity = 1;
+//             resolve()
+//         }, 300);
+//     })
     
-    .then(() => {
-        new Promise(resolve => {
-            setTimeout(() => {
-                header.style.opacity = 0;
-                resolve()
-            }, 1500);
-        })
+//     .then(() => {
+//         new Promise(resolve => {
+//             setTimeout(() => {
+//                 header.style.opacity = 0;
+//                 resolve()
+//             }, 1500);
+//         })
         
-        .then(() => {
-            new Promise(resolve => {
-                setTimeout(() => {
-                    header.innerHTML = "Color Organizer";
-                    header.style.opacity = 1;
-                    resolve()
-                }, 300);
-            });
-        });
-    });
-}
+//         .then(() => {
+//             new Promise(resolve => {
+//                 setTimeout(() => {
+//                     header.innerHTML = "Color Organizer";
+//                     header.style.opacity = 1;
+//                     resolve()
+//                 }, 300);
+//             });
+//         });
+//     });
+// }
 
 
 async function promiseAnimationAwait(text) {
