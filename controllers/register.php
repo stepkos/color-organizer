e66d11aaf0c12a068d2a6f48fe3447a5ebb5a974
stepkos <52202262+stepkos@users.noreply.github.com>
2021-01-08 03:28:02 +0100

    Register system

---
+++ controllers/register.php
@@ -0,0 +1,84 @@
+<?php
+
+// If user is login go to main page
+if (isset($_SESSION['logged'])) {
+    header("Location: index.php");
+    exit();
+}
+
+if (isset($_POST['email'])) {
+
+    // Flag do form is correct
+    $correctForm = true;
+
+    // Copy nick to validate
+    $nick = $_POST['nick'];
+
+    // Nick lenght validation
+    if ((strlen($nick) < 3) || (strlen($nick) > 20)) {
+        $correctForm = false;
+        $_SESSION['e_nick'] = "Nick must have from 3 to 20 characters!";
+    }
+
+    if (!ctype_alnum($nick)) {
+        $correctForm = false;
+        $_SESSION['e_nick'] = "Nick can hava only letters and digits!";
+    }
+
+    // E-mail adres validation
+    $email = $_POST['email'];
+    $emailB = filter_var($email, FILTER_SANITIZE_EMAIL);
+
+    if ((filter_var($emailB, FILTER_VALIDATE_EMAIL)==false) || ($emailB != $email)) {
+        $correctForm = false;
+        $_SESSION['e_email'] = "Incorrect e-mail adress";
+    }
+
+    // Password validate
+    $password1 = $_POST['password1'];
+    $password2 = $_POST['password2'];
+
+    if (strlen($password1) < 8 || strlen($password1) > 20) {
+        $correctForm = false;
+        $_SESSION['e_password'] = "Password must have from 8 to 20 characters!";
+    }
+
+    if ($password1 != $password2) {
+        $correctForm = false;
+        $_SESSION['e_password'] = "Passwords aren't the same";
+    }
+
+    $password_hash = password_hash($password1, PASSWORD_DEFAULT);
+
+    // Let's go to db
+    require_once "db/connect.php";
+
+    // Do email already exist
+    $result = $db->query("SELECT user_id FROM users WHERE email='$email'");
+
+    if ($result->rowCount() > 0) {
+        $correctForm = false;
+        $_SESSION['e_email'] = "Account with this adres already exist!";
+    }
+
+    // Do nick already exist
+    $result = $db->query("SELECT user_id FROM users WHERE login='$nick'");
+
+    if ($result->rowCount() > 0) {
+        $correctForm = false;
+        $_SESSION['e_nick'] = "Account with this nick already exist!";
+    }
+
+    if ($correctForm) {
+        $query = $db->prepare('INSERT INTO users VALUES (NULL, :login, :password, :email)');
+        $query->bindValue(':login', $nick, PDO::PARAM_STR);
+        $query->bindValue(':password', $password_hash, PDO::PARAM_STR);
+        $query->bindValue(':email', $email, PDO::PARAM_STR);
+        $query->execute();
+        
+        header('Location: login.php');
+        exit();
+    }
+        
+}
+
--- register.php
+++ register.php
@@ -2,8 +2,9 @@
 
     session_start();
 
-    require_once "templates/head_form.php";
+    require_once "controllers/register.php";
 
+    require_once "templates/head_form.php";
     require_once "templates/header_form.php";
     require_once "templates/mainOpen.php";
 
@@ -13,11 +14,32 @@
 
 <form method="post">
     
-    <input type="text" name="login" placeholder="login">
+    <input type="text" name="nick" placeholder="nick">
+
+    <?php
+        if (isset($_SESSION['e_nick'])) {
+            echo '<div class="error">'.$_SESSION['e_nick'].'</div>';
+            unset($_SESSION['e_nick']);    
+        }
+    ?>
 
     <input type="email" name="email" placeholder="email">
 
+    <?php
+        if (isset($_SESSION['e_email'])) {
+            echo '<div class="error">'.$_SESSION['e_email'].'</div>';
+            unset($_SESSION['e_email']);    
+        }
+    ?>
+
     <input type="password" name="password1" placeholder="password">
+
+    <?php
+        if (isset($_SESSION['e_password'])) {
+            echo '<div class="error">'.$_SESSION['e_password'].'</div>';
+            unset($_SESSION['e_password']);    
+        }
+    ?>
 
     <input type="password" name="password2" placeholder="password again">
 
@@ -27,5 +49,5 @@
 
 <?php
     require_once "templates/mainClose.php";
-    require_once "templates/footer.php";
+    require_once "templates/footer_form.php";
 ?>
